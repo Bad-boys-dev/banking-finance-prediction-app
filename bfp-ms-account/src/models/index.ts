76fe0cd976e4c14029430f13dde0bce375d643d9
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2025-05-09 19:50:35 +0100

    adding user model

--- bfp-ms-account/src/models/index.ts
+++ bfp-ms-account/src/models/index.ts
@@ -2,3 +2,4 @@ export { default as transaction } from './transaction.model';
 export { default as details } from './details.model';
 export { default as balance } from './balance.model';
 export { default as requisition } from './requisition.model';
+export { default as user } from './user.model';
---
+++ bfp-ms-account/src/models/user.model.ts
@@ -0,0 +1,16 @@
+import {
+  pgTable,
+  varchar,
+  text,
+} from 'drizzle-orm/pg-core';
+
+const userSchema = pgTable('users', {
+  id: text('id').primaryKey(),
+  username: varchar('username').notNull(),
+  firstName: varchar('firstName'),
+  surname: varchar('surname'),
+  email: varchar('email').notNull(),
+  password: varchar('password').notNull(),
+});
+
+export default userSchema;
